343e2c18a06c764ae60751b00b155b82e5bba257
Saksham <gupta.10@iitj.ac.in>
2018-03-19 21:33:23 +0530

    Added files
    Implemented Some Classes

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string.h>
+#include <graphics.h>
+
+using namespace std;
+
+int i;
+const char courses[6][6] = {'english','maths','physics','chemistry','HSS','CSE','Electrical','Mechanical'}
+char *student
+
+class Index{
+
+public:
+        void mainPage();
+        void welcome(char *text);
+
+};
+
+void homePageText()
+{
+
+    cout<<"Online Test Examination"<<endl<<"Created By"<<endl<<"B16CS025 -- Shreya Patel && B16CS030 -- Saksham Gupta"<<end;
+}
+
+void Index2::mainPage()
+{
+    textbackground(GREEN);
+    textcolor(RED);
+
+}
+
+class person{
+    protected:
+    string name;
+    int user_ID;
+    int phone;
+    string password;
+    string Email_id;
+    string first_name;
+    string last_name;
+};
+
+class student :: person{
+
+public:
+
+
+
+};
+
+
+
+
+class Quiz{
+    public:
+
+    void Edit();
+    void duration();
+    void No_of_questions();
+    void marks();
+    void test_id();
+    int display_result(char filename[10], int, int);
+    int edit_result(char filename[10], int);
+    int question_number;
+    int valid;
+
+
+};
+
+void Quiz :: display_result (char filename[10], int student_ID, int course_ID )
+{
+    fstream file;
+    file.open(filename, ios::in);
+    file.seekg(0, ios::beg);
+    while(file.read())
+    {
+
+    }
+}
+
+
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
