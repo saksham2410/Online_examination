8a2d7c266f95deb57d05aee60d14b15aa587058e
Saksham <gupta.10@iitj.ac.in>
2018-03-20 16:39:04 +0530

    Class Updates Part-1
    These are not final.

--- main.cpp
+++ main.cpp
@@ -31,6 +31,82 @@ void Index2::mainPage()
 
 }
 
+class user{
+private:
+    string UserId;
+    string Password;
+    string FirstName;
+    string LastName;
+public:
+    void login(){};
+    void viewProfile(){};
+    void register1(){ };
+};
+
+class student :: user {
+private:
+    struct appearedTest;
+public:
+    int getUserId(){};
+    void attemptTest(){};
+    void getReport(){};
+    void studentMenu(){};
+
+};
+
+class Professor :: user {
+private:
+    int courseID;
+public:
+    int getCourse(){};
+    void addQuestion(){};
+    void editQuestion(){};
+    void viewQuestion(){};
+    void professorMenu(){};
+    void getReport(){};
+};
+
+class Test{
+private:
+    int testID;
+    double marks;
+public:
+    void setTime(){};
+    void setTestID(){};
+    void getTestID(){};
+};
+
+class Question{
+private:
+    int questionNumber;
+    int type;
+    string question;
+    string choice1;
+    string choice2;
+    string choice3;
+    string choice4;
+    int correctAns;
+    double Posmarks;
+    double Negmarks;
+public:
+    void printQues(){};
+    int getCorrectAns(){};
+    int getQuesType(){};
+}
+
+class DatabaseManager{
+public:
+    struct getDetails(int){};
+    struct getReport(int){};
+    int getProfCourseId(int){};
+    bool validate(int){};
+    bool validate (int,string){};
+    void updateMarks(int,int,int){};
+    bool addStudent(Student){};
+
+
+
+
 class person{
     protected:
     string name;
