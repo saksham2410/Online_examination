4c2bda9c61d7b6bc83c8152fc60d6e2542e17dc4
Saksham <gupta.10@iitj.ac.in>
2018-03-25 03:53:34 +0530

    Added functions to let a user be able to give test

--- questions.h
+++ questions.h
@@ -75,9 +75,115 @@ void set_password(char pw[]){ strcpy(password, pw); }
 void set_app_file_name(char af[]){ strcpy(app_file_name, af); }
 void set_ques_file_name(char qf[]){ strcpy(ques_file_name, qf); }
 };
-//***************************************************************
-//    	FUNCTION TO CALCULATE NUMBER OF QUESTIONS IN THE FILE
-//***************************************************************
+
+
+void give_test(){
+Students test_applicant;
+  fstream fin;
+  fin.open(app_file_name,ios::in|ios::out|ios::binary);
+
+  calc_no_of_questions();
+  if(no_of_questions<no_of_ques_to_ask)
+  {
+    cout<<"********************************************************************************"<<endl;
+    cout<<"INSUFFICIENT QUESTION IN DATABASE. CONTACT ADMINISTRATOR"<<endl;
+    cout<<"********************************************************************************"<<endl;
+    cout<<"PRESS ANY KEY TO RETURN TO THE MAIN MENU";
+    getch();
+    main();
+  }
+  cout<<"ENTER YOUR APPLICANT NUMBER:"<<endl;
+  cin>>t_apno;
+  cout<<"ENTER DATE OF BIRTH"<<endl;
+  cout<<"ENTER DAY:";
+  cin>>t_dob_d;
+  cout<<"ENTER MONTH:";
+  cin>>t_dob_m;
+  cout<<"ENTER YEAR:";
+  cin>>t_dob_y;
+  int flag=0;
+  while (!fin.eof())
+  {
+    pos2=fin.tellg();
+    fin.read((char*)&test_applicant,sizeof(test_applicant));
+if(test_applicant.get_applicant_no()==t_apno&&test_applicant.get_dob_d()==t_dob_d&&test_applicant.get_dob_m()==t_dob_m&&test_applicant.get_dob_y()==t_dob_y)
+  {
+    flag=1;
+
+    cout<<endl;
+    break;
+  }
+  }
+  if(flag==1)
+  {
+    Questions t_obj;
+    ifstream t_ques;
+    t_ques.open(ques_file_name,ios::in|ios::binary);
+    time_t t;
+      int seedval= (unsigned) time (&t);
+       srand(seedval);
+    int marks=0;
+    int qno;
+    int ans;
+    for(int i=0; i<no_of_ques_to_ask;i++)
+    {
+      qno=rand()%no_of_questions+1;
+	    while(t_ques.read((char*)&t_obj, sizeof(t_obj)))
+	    {
+	      if(t_obj.get_id()==qno)
+	      {
+
+     cout<<"********************************************************************************"<<endl;
+		cout<<"Q"<<i+1<<". ";
+		t_obj.get_ques();
+    cout<<endl;
+    cout<<"********************************************************************************"<<endl;
+		cout<<endl;
+		cout<<"OPTIONS"<<endl<<"1. ";
+		t_obj.get_opt1();
+		cout<<endl;
+		cout<<"2. ";
+		t_obj.get_opt2();
+		cout<<endl;
+		cout<<"3. ";
+		t_obj.get_opt3();
+		cout<<endl;
+		cout<<"4. ";
+		t_obj.get_opt4();
+		cout<<endl;
+		cout<<"ENTER ANSWER: ( 1, 2, 3, 4 ) OR 0(ZERO TO SKIP THIS QUESTION:";
+		cin>>ans;
+		if (ans==0)
+		{
+		  ;
+		}
+		else if(t_obj.get_ans()==ans)
+		{ marks = marks + positive_mark;}
+		else{ marks = marks - negative_mark;}
+		break;
+	      }
+	    }
+      t_ques.seekg(0);
+    }
+    test_applicant.set_marks(marks);
+    fin.seekg(pos2);
+    fin.write((char*)&test_applicant,sizeof(test_applicant));
+    t_ques.close();
+    fin.close();
+  }
+  if(flag==0)
+  {
+
+    cout<<"********************************************************************************"<<endl;
+    cout<<"APPLICANT NOT FOUND!"<<endl;
+    cout<<"********************************************************************************"<<endl;
+  }
+  assign_rank();
+  cout<<"PRESS ANY KEY TO RETURN TO THE MAIN MENU";
+   getch(); main();
+ }
+
+
 void calc_no_of_questions(){
 
     //Define the function
